527374b038802793b528fb6a4572393ecf42d5ec
romelgomez <bmxandcode@gmail.com>
2023-07-04 10:08:07 -0500

    chore: cat example controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+class Animal {
+  name: string;
+  age: number;
+  breed: string;
+}
+
+class Cat extends Animal {
+  meow: string;
+
+  constructor(name: string, age: number, breed: string, meow: string) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+    this.meow = meow;
+  }
+}
+
+@Controller('animals')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('cats')
+  getCats(): Cat {
+    const cat = new Cat('Mittens', 2, 'Tabby', 'Meow');
+
+    return cat;
   }
 }
